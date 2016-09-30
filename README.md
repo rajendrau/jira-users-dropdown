c666668fea6264301145178017fd2c95683be11e
Rajendra Kumar U <rajendrau@digital-nirvana.com>
2016-09-30 17:25:42 +0530

    Script with users dropdown to filter the tasks.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jira-users-dropdown
 JIRA does not comes with Users dropdown. 
+
+jira-dashboard-userfilter.user.js is the TamperMonkey script.
+Update select_html code in the script with list of user's full names as per JIRA.
---
+++ jira-dashboard-userfilter.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         jira-dashboard-userfilter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Filter by User in JIRA dashboard
+// @author       Rajendra Kumar U
+// @include      http://jiraurlhere/secure/RapidBoard.jspa?rapidView=2*
+// @grant        none
+// ==/UserScript==
+
+//window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function() {
+    'use strict';
+    // your code here
+    
+    var $ = window.jQuery;
+    
+    function showAllTasks(){
+        $('.ghx-issue').each(function(){
+           $(this).show();
+        });
+        //alert(usr);
+    }
+    
+    
+    function showTasks(){
+        var usr = $('#usrs').val();
+        showAllTasks();
+        if(usr === ''){
+           return;
+        }
+        $('.ghx-issue').each(function(){
+           var img = $(this).find(".ghx-avatar-img");
+            // console.log(img.attr("title"), "Assignee: " + usr);
+            if(img.attr("title") != "Assignee: " + usr){
+              $(this).hide();
+           }
+        });
+        //alert(usr);
+    }
+    
+    
+    var select_html = `<select id='usrs'> \
+<option value=''>All</option>
+<option>User 1</option>
+<option>User 2</option>
+</select>`;
+    
+    $('#ghx-board-name').html($('#ghx-board-name').html() + select_html);
+    
+    $("#usrs").change(showTasks);
+
+    
+});
